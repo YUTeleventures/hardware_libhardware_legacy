c167eac74a15d0d4cb7de1b27b5220726784aa03
Louis Popi <theh2o64@gmail.com>
2016-06-28 16:13:57 +0200

    Revert "wifi: fix samsung wifi logic"
    * We aren't SAMSUNG
    
    This reverts commit 34ce83ef2993764fab2d6c5511acf94bd9d5392d.
    
    Change-Id: Ifd5e8fc6e212ba323c485d86330f99280c0645f4

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -322,13 +322,16 @@ int wifi_load_driver()
     int count = 100; /* wait at most 20 seconds for completion */
     char module_arg2[256];
 #ifdef SAMSUNG_WIFI
-    char* type = get_samsung_wifi_type();
-
+#ifdef WIFI_DRIVER_MODULE_AP_ARG
     if (wifi_mode == 1) {
-        snprintf(module_arg2, sizeof(module_arg2), "%s%s", DRIVER_MODULE_AP_ARG, type == NULL ? "" : type);
+        snprintf(module_arg2, sizeof(module_arg2), DRIVER_MODULE_AP_ARG);
     } else {
-        snprintf(module_arg2, sizeof(module_arg2), "%s%s", DRIVER_MODULE_ARG, type == NULL ? "" : type);
+        snprintf(module_arg2, sizeof(module_arg2), DRIVER_MODULE_ARG);
     }
+#else
+    char* type = get_samsung_wifi_type();
+    snprintf(module_arg2, sizeof(module_arg2), "%s%s", DRIVER_MODULE_ARG, type == NULL ? "" : type);
+#endif
 
     if (insmod(DRIVER_MODULE_PATH, module_arg2) < 0) {
 #else
